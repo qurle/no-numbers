202bc34cca48016e254f31e62dad2f73d7c87ee1
nick <32414396+qurle@users.noreply.github.com>
2021-09-30 00:59:53 +0300

    made messages bettor
    i learnt how to increment so now i can tell you if no layers were afftected

--- code.js
+++ code.js
@@ -1,5 +1,6 @@
-// I really don't care about quality of the code, but you can fork it to make it better
-const finishMsgs = ["Done!", "You got it!", "All renamed!", "Is that all?", "My job here is done", "Gotcha!", "It wasn't hard", "Got it! What's next?"];
+// I really don't care about quality of the code, but you can make pull request to make it bettor!
+let finishMsgs = ["Done!", "You got it!", "All renamed!", "Is that all?", "My job here is done", "Gotcha!", "It wasn't hard", "Got it! What's next?"];
+const finishVain = ["No numbers, already", "I see no layers with numbers", "Any default numbers? I can't see it", "Nothing to do, your layers are great"];
 const dict = [
     "Frame",
     "Group",
@@ -10,13 +11,14 @@ const dict = [
     "Ellipse",
     "Polygon",
     "Star",
-    "Vector"
+    "Vector",
+    "Component"
 ];
 const regex = /^\w+(?= \d+)|\d+$/gm;
 let notification;
 let selection;
 let working;
-let count;
+let count = 0;
 figma.on("currentpagechange", escape);
 // Main + Elements Check
 const nodes = [];
@@ -38,8 +40,10 @@ function recursiveRename(node) {
     if (node.type !== "PAGE") {
         const match = node.name.match(regex);
         const index = (match && match.length === 2) ? dict.indexOf(match[0]) : -1;
-        if (index >= 0)
+        if (index >= 0) {
+            count++;
             node.name = dict[index];
+        }
     }
     if ("children" in node) {
         for (const child of node.children) {
@@ -61,7 +65,15 @@ function escape() {
 }
 function finish() {
     working = false;
-    notify(finishMsgs[Math.floor(Math.random() * finishMsgs.length)]);
+    if (count > 0) {
+        if (count === 1)
+            finishMsgs.push("Renamed only one layer");
+        else
+            finishMsgs.push("Renamed " + count + "layers in total");
+        notify(finishMsgs[Math.floor(Math.random() * finishMsgs.length)]);
+    }
+    else
+        notify(finishVain[Math.floor(Math.random() * finishVain.length)]);
     //figma.viewport.scrollAndZoomIntoView(selection)
     figma.closePlugin();
 }
